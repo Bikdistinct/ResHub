809300d1a8d351d9a4b8dd1ec914c8c397b6ad7e
abhishek nath <abhishek78nath@gmail.com>
2021-12-03 20:02:34 +0530

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+let burger = document.getElementById('burger');
+let nav = document.getElementById('nav');
+// let burger = document.querySelector('.burger');
+
+burger.addEventListener('click',()=>{
+    nav.classList.toggle('respnav')
+})
